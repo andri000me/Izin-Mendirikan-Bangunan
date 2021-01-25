508862a7b3538a22093cefff9a70c5cb56b9ba82
asiah77 <77655674+asiah77@users.noreply.github.com>
2021-01-25 09:47:52 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead><hr>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM PERIZINAN<br> MENDIRIKAN BANGUNAN</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="justify">
+
+                          </div>
+                          <div class="col-sm-8" align="">
+                            <br>
+                  
+                                <p>Sistem Izin Mendirikan Bangunan atau biasa dikenal dengan IMB adalah 
+                            sebuah produk hukum yang berisi perizinan yang diberikan oleh Kepala Daerah 
+                            kepada pemilik bangunan untuk membangun baru, mengubah, memperluas, mengurangi, 
+                            merawat atau merobohkan bangunan sesuai dengan persyaratan administratif 
+                            dan persyaratan teknis yang berlaku.[1] </p>
+
+                            <p>IMB merupakan salah satu produk hukum untuk mewujudkan tatanan tertentu
+                            sehingga tercipta ketertiban, keamanan, keselamatan, kenyamanan, 
+                            sekaligus kepastian hukum. Kewajiban setiap orang atau badan yang akan 
+                            mendirikan bangunan untuk memiliki Izin Mendirikan Bangunan diatur
+                            pada Pasal 5 ayat 1 Perda 7 Tahun 2009.[2]</p>
+
+                            <p>IMB akan melegalkan suatu bangunan yang direncanakan sesuai dengan Tata Ruang yang telah ditentukan. 
+                            Selain itu, adanya IMB menunjukkan bahwa rencana konstruksi bangunan tersebut juga dapat dipertanggungjawabkan
+                            dengan maksud untuk kepentingan bersama.[2] </p>
+
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="justify">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
